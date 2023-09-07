05ca75af5214043721a61784dde6efcc5bbbdf45
ayaman <ayaman@example.com>
2023-09-07 00:48:40 +0000

    top画面書いた

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//タスク管理システム
+Route::get('/', [AuthController::class, 'index']);
+

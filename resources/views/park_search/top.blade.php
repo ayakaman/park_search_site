0270b522c1266ef9f6ee4c2467f9a60eb4162bf2
ayaman <ayaman@example.com>
2023-09-08 03:42:03 +0000

    ログインページ

--- resources/views/park_search/top.blade.php
+++ resources/views/park_search/top.blade.php
@@ -4,16 +4,26 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>公園検索 mypage</title>
-        <link rel="stylesheet" type="text/css" href="css/style.css">
+        <link rel="stylesheet" type="text/css" href="/css/style.css">
     </head>
     <body>
       <header>
         <h1 class="heading">
+          <img src="/images/park.png">
         </h1>
       </header>
        <div class="main">
-         <p>「楽しい」を　見つけよう</p>
+         <img src="/images/t1.png">
+         <img src="/images/t2.png">
        </div>
+       <div class="main-visual">
+         <div class="mv-search"></div>
+         <div class="mv-text"></div>
+       </div>
+
+       <footer>
+         <p class="footer-text">©2023</p>
+       </footer>
 
     </body>
 </html>
